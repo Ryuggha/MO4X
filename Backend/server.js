a12e6b6f5ea4005b1395e82740d074c5957ed53b
Ryuggha <rogerperalesmares493@gmail.com>
2022-03-22 20:17:38 +0100

    Basic LogIn Complete

--- Backend/server.js
+++ Backend/server.js
@@ -3,9 +3,46 @@ const keys = require('./config/keys.js')
 
 const app = express();
 
+// Setting Up DB
 const mongoose = require('mongoose');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
+//Setip database models
+require('./model/Account')
+const Account = mongoose.model('accounts');
+
+// Routes
+app.get('/account', async (req, res) => {
+
+    const { reqUsername, reqPassword} = req.query;
+    if (reqUsername == null || reqPassword == null) {
+        res.send("Invalid Credentials");
+        return;
+    }
+
+    var userAccount = await Account.findOne( {username : reqUsername} );
+    if (userAccount == null) {
+        var newAccount = new Account({
+            username: reqUsername,
+            password: reqPassword,
+
+            lastAuthentication: Date.now()
+        });
+        await newAccount.save();
+
+        res.send(newAccount);
+        return;
+    } else {
+        if (reqPassword == userAccount.password) {
+            userAccount.lastAuthentication = Date.now();
+            await userAccount.save();
+            res.send(userAccount)
+            return;
+        }
+    }
+});
+
+// Listen
 const port = keys.port;
 app.listen(port, () => {
     console.log("Listening on " + port);
